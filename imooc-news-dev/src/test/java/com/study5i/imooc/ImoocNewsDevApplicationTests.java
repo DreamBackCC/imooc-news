5247f5f567ece54088df09b8ed8f5878cf74cd6a
kangdu <du.kang@sunyard.com>
2022-09-04 17:03:22 +0800

    项目初始化

---
+++ imooc-news-dev/src/test/java/com/study5i/imooc/ImoocNewsDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study5i.imooc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImoocNewsDevApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
